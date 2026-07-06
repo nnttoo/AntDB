6071c868b137b0be63772e8e201bfa4a3347fd1c
Anto <aaaannnnttttoooo@gmail.com>
2026-07-06 11:49:12 +0800

    doc: add benchmark hget

--- redisBenchMark/docker_entry/entrypoint_run.sh
+++ redisBenchMark/docker_entry/entrypoint_run.sh
@@ -11,7 +11,8 @@ sleep 5
 
 echo "=== 3. Running Node.js Functional Tests ===" 
 # Output test Node.js langsung dialihkan ke terminal (tidak ke log file)
-cp /nodeTester/src/index.ts  /usr/src/tester/src/index.ts
+# cp /nodeTester/src/index.ts  /usr/src/tester/src/index.ts
+cp -r /nodeTester/src /usr/src/tester/
 cd /usr/src/tester && npx tsx ./src/index.ts  
 
 echo "=========================================="
@@ -22,7 +23,14 @@ echo "=========================================="
 echo "Running all benchmarks (PING, SET, GET, HSET) to CSV..."
 
 # Menjalankan semua pengujian sekaligus dan menyimpannya ke satu file CSV
-redis-benchmark -h 127.0.0.1 -p 6379 -c 10 -n 50000 -t  ping,set,get,hset,hget --csv > /working_dir/benchmark_results.csv
+redis-benchmark -h 127.0.0.1 -p 6379 -c 10 -n 50000 -t  ping,set,get,hset --csv > /working_dir/benchmark_results.csv
+ 
+
+echo "Running custom HGET benchmark..."
+# 2. Use append (>>) to add the custom HGET test to the same CSV file
+# Use -r 10000 so the requested keys/fields are randomized according to the HSET data
+redis-benchmark -h 127.0.0.1 -p 6379 -c 10 -n 50000 -r 10000 --csv HGET "myhash:__rand_int__" "counter:__rand_int__" >> /working_dir/benchmark_results.csv
+
 
 echo "\n=== Tests Completed. All benchmark results saved to /working_dir/benchmark_results.csv ==="
 
