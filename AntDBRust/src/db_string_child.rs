ccb754b8e2f0daa51819f70c4d8f6fc5f5df3de0
Anto <aaaannnnttttoooo@gmail.com>
2026-07-11 11:39:31 +0800

    refactor: remove no need fn

--- AntDBRust/src/db_string_child.rs
+++ AntDBRust/src/db_string_child.rs
@@ -12,17 +12,7 @@ impl AntDBStringChild {
         Self {
             string_child: Arc::new(RwLock::new(value)),
         }
-    }
-
-    pub fn set(&self, value: String) -> Result<(), BoxError> {
-        let Ok(mut str_write) = self.string_child.write() else {
-            return Err(Box::from("error lock hchild"));
-        };
-
-        *str_write = value;
-
-        Ok(())
-    }
+    } 
 
     pub fn get(&self) -> Result<String, BoxError> {
         let Ok(str_read) = self.string_child.read() else {
